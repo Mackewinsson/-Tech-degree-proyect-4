0eaaa18221bb4bbcf96c6a1993a1a60e95ec30f2
Mackewinsson <Mackewinsson@gmail.com>
2020-05-08 08:56:19 -0400

    Part 9 almost finished

--- js/Game.js
+++ js/Game.js
@@ -32,4 +32,82 @@
         const ramdomIndex = Math.floor(Math.random()*(4+1));
         return this.phrases[ramdomIndex];
     };
- };
+/**
+* Begins game by selecting a random phrase and displaying it to user
+*/
+    startGame() {
+
+        // Hide the overlay 
+
+        const overlay = document.querySelector('div#overlay');
+        overlay.style.display = 'none';
+
+        // Call getRamdomPhrase 
+        
+        //
+        this.activePhrase = this.getRandomPhrase();
+        this.activePhrase.addPhraseToDisplay();
+        console.log(this.activePhrase);
+
+
+    };
+
+/**
+* Checks for winning move
+* @return {boolean} True if game has been won, false if game wasn't
+won
+*/
+    checkForWin() {
+
+        const letterList = document.querySelector('#phrase > ul').children;
+        let count = 0;
+        let test;
+
+        for(let i = 0; i < letterList.length; i++){
+
+            if(letterList[i].classList.contains('show')){
+                count++;
+            };
+        };
+
+        if(count == letterList.length){
+            return true;
+        } else{
+            return false;
+        };
+    };
+
+/**
+* Increases the value of the missed property
+* Removes a life from the scoreboard
+* Checks if player has remaining lives and ends game if player is out
+*/
+    removeLife() {
+
+        const lives = document.querySelectorAll('div#scoreboard > ol > li > img');   
+        lives[this.missed].setAttribute('src', 'images/lostHeart.png');
+        this.missed++;
+
+        if(this.missed > 4){
+            this.gameOver(false);
+        };
+    };
+/**
+* Displays game over message
+* @param {boolean} gameWon - Whether or not the user won the game
+*/
+    gameOver(gameWon) {
+        const h1 = document.querySelector('#game-over-message');
+        const overlay = document.querySelector('div#overlay');
+
+        if(gameWon == true){
+            h1.textContent = 'Congratulation! You win!!!';
+            overlay.className = 'win'; 
+        } else if (gameWon == false){
+            h1.textContent = 'You lost, try again';
+            overlay.className = 'lose'            
+        }
+    };
+};
+
+ 
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,11 +1,77 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
- * Phrase.js */
+ * Phrase.js *
+ * Display phrase on game board
+*/
 
- class Phrase{
-    constructor(phrase){
-      this.phrase = phrase.toLowerCase();
+class Phrase{
+
+  constructor(phrase){
+    this.phrase = phrase.toLowerCase();
         
-    }
+  };
 
- };
+  addPhraseToDisplay() {
+
+    const phraseDiv = document.querySelector('#phrase');
+    const ul = phraseDiv.firstElementChild;
+
+    const splitPhrase = this.phrase
+    .split('');
+    // Log splited phrase to console
+    console.log(splitPhrase);
+
+    // Forloop to add each item to the DOM
+    for(let i = 0; i < splitPhrase.length; i++){
+      let letter = document.createElement('li');
+      letter.textContent = splitPhrase[i];
+      // to check if the content is letter or space
+      if(splitPhrase[i] != ' '){
+        letter.className = `hide letter ${splitPhrase[i]}`;
+        ul.appendChild(letter);
+      } else{
+        letter.className = `hide space`;
+        ul.appendChild(letter);
+      };
+    };
+  };
+/**
+* Checks if passed letter is in phrase
+* @param (string) letter - Letter to check
+*/
+  checkLetter(letter) {
+
+    let test = false;
+    let checkPhrase = this.phrase.split('');
+
+    for(let i = 0; i < checkPhrase.length; i++){
+
+      if(checkPhrase[i] == letter){
+        test = true;
+      };
+    };
+    return test;
+  };
+
+/**
+* Displays passed letter on screen after a match is found
+* @param (string) letter - Letter to display
+*/
+  showMatchedLetter(letter) {
+
+    const phraseDiv = document.querySelector('#phrase');
+    const ul = phraseDiv.firstElementChild;
+    let li = ul.children;
+
+    for(let i = 0; i < li.length; i++){
+
+      let element = li[i];
+      if(element.className == `hide letter ${letter}`){
+        element.className = `show letter ${letter}`;
+      };
+
+    };
+
+  };
+
+};
